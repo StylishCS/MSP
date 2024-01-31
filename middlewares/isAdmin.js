faf396e4d37fea0331832c26cfb69a4089b36da7
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-01-31 04:46:50 +0200

    debugging error

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,27 +1,37 @@
 const { Admin } = require("../models/Admin");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-async function AdminPrivileges(req,res,next){
-    try {
-        if (!req.header("Authorization")) {
-            return res.status(401).json("FORBIDDEN");
-        }
-        const key = req.header("Authorization").split(" ")[0];
-        const token = req.header("Authorization").split(" ")[1];
-        if(key !== "MSP"){
-            return res.status(401).json("FORBIDDEN");
-        }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const admin = Admin.findById(decoded._id);
-        if(!admin[0]){
-            return res.status(401).json("FORBIDDEN");
-        }
-        req.adminId = decoded._id;
-        console.log(req.adminId);
-        next();
-    } catch (error) {
-        return res.status(401).json("FORBIDDEN");
+async function AdminPrivileges(req, res, next) {
+  try {
+    if (!req.header("Authorization")) {
+      console.log("flag 1");
+      console.log(req.header("Authorization"));
+      return res.status(401).json("FORBIDDEN");
     }
+    const key = req.header("Authorization").split(" ")[0];
+    const token = req.header("Authorization").split(" ")[1];
+    if (key !== "MSP") {
+      console.log("flag 2");
+      console.log(key);
+      console.log(token);
+      return res.status(401).json("FORBIDDEN");
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const admin = Admin.findById(decoded._id);
+    if (!admin[0]) {
+      console.log("flag 3");
+      console.log(admin);
+      console.log(admin[0]);
+      return res.status(401).json("FORBIDDEN");
+    }
+    req.adminId = decoded._id;
+    console.log(req.adminId);
+    next();
+  } catch (error) {
+    console.log("flag 4");
+    console.log(error);
+    return res.status(401).json("FORBIDDEN");
+  }
 }
 
 module.exports = AdminPrivileges;

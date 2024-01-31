062a2d5c1db9183c2d70770d81bd741fb376043e
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-01-31 04:55:09 +0200

    debugging

--- controllers/adminLoginController.js
+++ controllers/adminLoginController.js
@@ -15,12 +15,13 @@ async function adminLoginController(req, res) {
     if (!(await bcrypt.compare(req.body.password, user[0].password))) {
       return res.status(401).json("Wrong Email Or Password...");
     }
+    console.log(user._id);
     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRE,
     });
-    return res.status(200).json({user, token});
+    return res.status(200).json({ user, token });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res.status(500).json("INTERNAL SERVER ERROR");
   }
 }

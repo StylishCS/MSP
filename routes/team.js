86020e5f548dcff7a19f3a5de3e6698af8087f67
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-01-31 08:08:27 +0200

    resolve syntax error

--- routes/team.js
+++ routes/team.js
@@ -1,7 +1,7 @@
+var express = require("express");
 const app = require("../app");
 var router = express.Router();
 
-var express = require("express");
 const {
   addTeamMember,
   getTeamMembers,
@@ -12,7 +12,7 @@ const paginateData = require("../middlewares/paginateData");
 const { TeamMember } = require("../models/TeamMember");
 
 router.post("/add", upload.single("image"), addTeamMember);
-router.get("/get",paginateData(TeamMember), getTeamMembers);
+router.get("/get", paginateData(TeamMember), getTeamMembers);
 router.delete("/delete/:id", deleteTeamMember);
 
 module.exports = router;

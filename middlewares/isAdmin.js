04c986cd4b8e8e6718153f7d634f1494dfc99710
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-01-31 04:58:13 +0200

    resolving admin protection

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -4,36 +4,21 @@ const jwt = require("jsonwebtoken");
 async function AdminPrivileges(req, res, next) {
   try {
     if (!req.header("Authorization")) {
-      console.log("flag 1");
-      console.log(req.header("Authorization"));
       return res.status(401).json("FORBIDDEN");
     }
     const key = req.header("Authorization").split(" ")[0];
     const token = req.header("Authorization").split(" ")[1];
     if (key !== process.env.JWT_KEYWORD) {
-      console.log("flag 2");
-      console.log(key);
-      console.log(token);
       return res.status(401).json("FORBIDDEN");
     }
-    console.log(key)
-    console.log(token)
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const admin = Admin.findById(decoded._id);
-    console.log(admin)
+    const admin = await Admin.findById(decoded._id);
     if (!admin[0]) {
-      console.log("flag 3");
-      console.log(decoded._id)
-    //   console.log(admin);
-    //   console.log(admin[0]);
       return res.status(401).json("FORBIDDEN");
     }
     req.adminId = decoded._id;
-    console.log(req.adminId);
     next();
   } catch (error) {
-    console.log("flag 4");
-    console.log(error);
     return res.status(401).json("FORBIDDEN");
   }
 }

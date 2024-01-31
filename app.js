3d8074ba29557b126b75d63edf30223f8807a7a5
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-01-31 05:55:58 +0200

    serve static files

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "uploads")));
 
 /* Routes */
 app.use("/", indexRouter);

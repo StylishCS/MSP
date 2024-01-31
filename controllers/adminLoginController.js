1f69a163f6792c134a2d5622793304fe2e7e127b
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-01-31 04:21:21 +0200

    Debugging error

--- controllers/adminLoginController.js
+++ controllers/adminLoginController.js
@@ -20,6 +20,7 @@ async function adminLoginController(req, res) {
     });
     return res.status(200).json(user, token);
   } catch (error) {
+    console.log(error)
     return res.status(500).json("INTERNAL SERVER ERROR");
   }
 }

2c726adec73d4096ea5255d2699bf50db4027032
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-02-01 02:41:27 +0200

    gallery client side

--- routes/galleryClient.js
+++ routes/galleryClient.js
@@ -2,12 +2,16 @@ var express = require("express");
 const {
   getGalleryItems,
   getGalleryItemById,
+  getEventsGallery,
+  getSessionsGallery
 } = require("../controllers/galleryController");
 const { Gallery } = require("../models/Gallery");
 
 var router = express.Router();
 
 router.get("/get", paginatedResults(Gallery), getGalleryItems);
+router.get("/getEvents", getEventsGallery);
+router.get("/getSessions", getSessionsGallery);
 router.get("/getById/:id", getGalleryItemById);
 
 /* Pagination Function */

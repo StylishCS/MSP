41d4aab91034cd14930a8f4f87feafa07dee6b26
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-07-24 22:56:11 +0300

    modify login api response

--- controllers/adminLoginController.js
+++ controllers/adminLoginController.js
@@ -8,14 +8,14 @@ async function adminLoginController(req, res) {
     if (!req.body.email || !req.body.password) {
       return res.status(400).json("No Email Or Password Provided...");
     }
-    const user = await Admin.find({ email: req.body.email });
-    if (!user[0]) {
+    const user = await Admin.findOne({ email: req.body.email });
+    if (!user) {
       return res.status(401).json("Wrong Email Or Password...");
     }
-    if (!(await bcrypt.compare(req.body.password, user[0].password))) {
+    if (!(await bcrypt.compare(req.body.password, user.password))) {
       return res.status(401).json("Wrong Email Or Password...");
     }
-    const token = jwt.sign({ _id: user[0]._id }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRE,
     });
     return res.status(200).json({ user, token });
